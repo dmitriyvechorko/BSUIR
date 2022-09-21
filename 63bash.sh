3fc1ed28f504a48329e6ce087a1331c679a9134c
Dmitry Vechorko <113061286+dmitriyvechorko@users.noreply.github.com>
2022-09-21 19:28:41 +0300

    Add files via upload

---
+++ 63bash.sh
@@ -0,0 +1,13 @@
+#!bin/bash/
+read -p "Vvedite put " x
+read -p "Vvedite adres " y
+if [ ! -d $x ]; then
+  exit
+fi
+if [ -f *.exe ]; then
+  ping $y > log.txt
+else
+  echo > Process.txt
+  ps aux | wc -l > Process.txt
+ fi
+$SHELL
