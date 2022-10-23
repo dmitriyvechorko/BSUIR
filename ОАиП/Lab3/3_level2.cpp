5b5d03c46d4a7f0ae2959ba8a6b9f44790ea6520
Dmitry Vechorko <113061286+dmitriyvechorko@users.noreply.github.com>
2022-10-23 23:01:42 +0300

    Update 3_level2.cpp

--- ОАиП/Lab3/3_level2.cpp
+++ ОАиП/Lab3/3_level2.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 float func2(float x) {
-	return (pow(2.7182818284590, x * cos(M_PI / 4)) * cos(sin(x)));
+	return (pow(2.7182818284590, x * cos(M_PI / 4)) * cos(x*sin(M_PI/4)));
 }
 
 long double fact2(int N)
@@ -18,23 +18,25 @@ long double fact2(int N)
 int main() {
 	float a, b, h, summ;
 	int i = 1, n = 0;
-	cout << "Pls input a,b,h,n \n";
-	cin >> a >> b >> h >> n;
-	cout << "\n";
+	cout << "Pls input a, b, h, n\n";
 
-	while (!(cin >> a >> b >> h >> n) || (cin.peek() != '\n')) {
-         
-	 cin.clear();
-         while (cin.get() != '\n');
-         cout << "Error!" << endl;
-        }
+	while (!(cin >> a, b, h, n) || (cin.peek() != '\n'))
+	{
+		cin.clear();
+		while (cin.get() != '\n');
+		cout << "Error!" << endl;
+	}
+	
+	cout << "\n";
 
 	cout << "i\t  Y\t S\t |Y-S|\n";
 
-	for (a; a <= b; a += h, i++) {
+	for (a; a <= b; a += h, i++) 
+	{
 		summ = 0;
-		for (int k = 0; k <= n; k++) {
-			summ += (cos(k * M_PI / 4) * pow(a , k) / fact2(k));
+		for (int k = 0; k <= n; k++) 
+		{
+			summ += (cos(k * M_PI / 4) * pow(a, k) / fact2(k));
 		}
 		cout << i << "\t" << func2(a) << "\t" << summ << "\t" << abs(func2(a) - summ) << "\n";
 	}
